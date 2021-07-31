8dc7e559b2c907c7697702d770fbdcef2d6e08c5
GaliaBonda <galina.rizhniak@gmail.com>
2021-08-01 01:31:35 +0300

    add files

---
+++ build/index.js
@@ -0,0 +1,130 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const runtypes_1 = require("runtypes");
+//summ :: BigObject → number
+function summ(a) {
+    const x = Object.keys(a).map((k) => {
+        var _a;
+        const elem = a[k];
+        if (elem === undefined) {
+            return 0;
+        }
+        if (typeof elem.cvalue === 'string') {
+            return +elem.cvalue || 2021;
+        }
+        if (isBigObject(elem.cvalue)) {
+            return summ(elem.cvalue);
+        }
+        return (_a = elem.cvalue) !== null && _a !== void 0 ? _a : 2021;
+    });
+    let sum = 0;
+    for (let i = 0; i < x.length; i++) {
+        sum += x[i];
+    }
+    return sum;
+}
+function isBigObject(obj) {
+    if (typeof obj !== 'object' || !obj)
+        return false;
+    //const keys: string[] = Object.keys(obj);
+    const CValueObj = runtypes_1.Lazy(() => runtypes_1.Record({
+        cvalue: runtypes_1.Union(runtypes_1.String, runtypes_1.Number, runtypes_1.Undefined, BigObject),
+    }));
+    const BigObjValue = runtypes_1.Union(runtypes_1.Undefined, CValueObj);
+    const a = {};
+    const keys = Object.keys(obj);
+    // if (!hasKey(obj, keys[0])) return false;
+    // const newObj = obj[keys[0]];
+    // const cvalue = 'cvalue';
+    // if (hasKey(newObj, cvalue) && typeof newObj[cvalue] === 'object') {
+    //   keys = Object.keys(newObj[cvalue]);
+    //   console.log('we are here');
+    // }
+    console.log('keys ', keys);
+    for (let i = 0; i < keys.length; i++) {
+        a[keys[i]] = BigObjValue;
+    }
+    const BigObject = runtypes_1.Record(a);
+    try {
+        BigObject.check(obj);
+        return true;
+    }
+    catch (e) {
+        console.log('Oops...', e);
+        return false;
+    }
+}
+function hasKey(obj, key) {
+    return key in obj;
+}
+const example = {
+    hello: { cvalue: 1 },
+    world: {
+        cvalue: { yay: { cvalue: '2' } },
+    },
+};
+const example1 = {
+    hello: { cvalue: 1 },
+    world: {
+        cvalue: { yay: { cvalue: '2' } },
+    },
+    again: {
+        cvalue: {
+            yak: {
+                cvalue: { good: { cvalue: 3 } },
+            },
+        },
+    },
+};
+const example2 = {
+    hello: { cvalue: 1 },
+    world: {
+        cvalue: { yay: { cvalue: 'gg' } },
+    },
+    again: {
+        cvalue: {
+            yay: {
+                cvalue: { good: { cvalue: 3 } },
+            },
+        },
+    },
+};
+const example3 = {
+    hello: { cvalue: 1 },
+    world: {
+        cvalue: { yay: { cvalue: 'gg' } },
+    },
+    again: {
+        cvalue: {
+            ha: {
+                cvalue: { good: { cvalue: undefined } },
+            },
+        },
+    },
+};
+const example4 = {
+    hello: { cvalue: 1 },
+    world: {
+        cvalue: undefined,
+    },
+};
+const example5 = {
+    mobo: undefined,
+    hello: { cvalue: 1 },
+    world: {
+        cvalue: {
+            yay: { cvalue: '2' },
+            grgr: { cvalue: undefined },
+            grr: { cvalue: '1q' },
+            qq: undefined,
+        },
+    },
+    grgr: { cvalue: undefined },
+};
+console.log(summ(example));
+console.log(summ(example1));
+console.log(summ(example2));
+console.log(summ(example3));
+console.log(summ(example4));
+console.log(summ(example5));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,146 @@
+import { Number, String, Record, Union, Undefined, Lazy } from 'runtypes';
+import { RuntypeBase } from 'runtypes/lib/runtype';
+
+interface BigObject {
+  [a: string]:
+    | {
+        cvalue: number | string | undefined | BigObject;
+      }
+    | undefined;
+}
+
+//summ :: BigObject → number
+
+function summ(a: BigObject): number {
+  const x = Object.keys(a).map((k) => {
+    const elem = a[k];
+    if (elem === undefined) {
+      return 0;
+    }
+    if (typeof elem.cvalue === 'string') {
+      return +elem.cvalue || 2021;
+    }
+    if (isBigObject(elem.cvalue)) {
+      return summ(elem.cvalue as BigObject);
+    }
+    return elem.cvalue ?? 2021;
+  });
+  let sum = 0;
+  for (let i = 0; i < x.length; i++) {
+    sum += x[i] as number;
+  }
+  return sum;
+}
+
+function isBigObject(obj: unknown): boolean {
+  if (typeof obj !== 'object' || !obj) return false;
+  //const keys: string[] = Object.keys(obj);
+  const CValueObj: RuntypeBase<unknown> = Lazy(() =>
+    Record({
+      cvalue: Union(String, Number, Undefined, BigObject),
+    }),
+  );
+  const BigObjValue = Union(Undefined, CValueObj);
+  const a: { [l: string]: typeof BigObjValue } = {};
+  const keys: string[] = Object.keys(obj);
+  // if (!hasKey(obj, keys[0])) return false;
+  // const newObj = obj[keys[0]];
+  // const cvalue = 'cvalue';
+  // if (hasKey(newObj, cvalue) && typeof newObj[cvalue] === 'object') {
+  //   keys = Object.keys(newObj[cvalue]);
+  //   console.log('we are here');
+  // }
+  console.log('keys ', keys);
+  for (let i = 0; i < keys.length; i++) {
+    a[keys[i]] = BigObjValue;
+  }
+  const BigObject = Record(a);
+  try {
+    BigObject.check(obj);
+    return true;
+  } catch (e) {
+    console.log('Oops...', e);
+    return false;
+  }
+}
+
+// function hasKey<O>(obj: O, key: PropertyKey): key is keyof O {
+//   return key in obj;
+// }
+
+const example: BigObject = {
+  hello: { cvalue: 1 },
+  world: {
+    cvalue: { yay: { cvalue: '2' } },
+  },
+};
+
+const example1: BigObject = {
+  hello: { cvalue: 1 },
+  world: {
+    cvalue: { yay: { cvalue: '2' } },
+  },
+  again: {
+    cvalue: {
+      yak: {
+        cvalue: { good: { cvalue: 3 } },
+      },
+    },
+  },
+};
+
+const example2: BigObject = {
+  hello: { cvalue: 1 },
+  world: {
+    cvalue: { yay: { cvalue: 'gg' } },
+  },
+  again: {
+    cvalue: {
+      yay: {
+        cvalue: { good: { cvalue: 3 } },
+      },
+    },
+  },
+};
+
+const example3: BigObject = {
+  hello: { cvalue: 1 },
+  world: {
+    cvalue: { yay: { cvalue: 'gg' } },
+  },
+  again: {
+    cvalue: {
+      ha: {
+        cvalue: { good: { cvalue: undefined } },
+      },
+    },
+  },
+};
+
+const example4: BigObject = {
+  hello: { cvalue: 1 },
+  world: {
+    cvalue: undefined,
+  },
+};
+
+const example5: BigObject = {
+  mobo: undefined,
+  hello: { cvalue: 1 },
+  world: {
+    cvalue: {
+      yay: { cvalue: '2' },
+      grgr: { cvalue: undefined },
+      grr: { cvalue: '1q' },
+      qq: undefined,
+    },
+  },
+  grgr: { cvalue: undefined },
+};
+
+console.log(summ(example));
+console.log(summ(example1));
+console.log(summ(example2));
+console.log(summ(example3));
+console.log(summ(example4));
+console.log(summ(example5));
